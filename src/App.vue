b7b535f259900bf4e807e1d13e526cd8dc61ae7d
Janarththan4 <ssjanarththan.4@gmail.com>
2022-05-30 12:17:39 -0700

    FetchJSONandDisplay

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+ 
+    <div>{{employees}}fdfdf</div>
+</div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      employees: []
+  }
+  },
+  methods: {
+   getData(){
+     debugger
+     console.log("name");
+   },
+    getEmployeeData() {
+      debugger
+      fetch("db.json")
+        .then(response => response.json())
+        .then(data => (this.employees = data));
+  },
+  
+} ,
+created() {
+  this.getEmployeeData()
+  },
+}
+</script>
