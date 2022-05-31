3e03d6827b55c28ae495e2ade95e3026cd0daf11
Janarththan4 <ssjanarththan.4@gmail.com>
2022-05-31 07:21:07 -0700

    GET json data and DISPLAY

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-<div>
- 
-    <div>{{employees}}fdfdf</div>
-</div>  
+<table>
+  <tr>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Gender</td>
+    <td>Age</td>
+    <td>Email</td>
+  </tr>
+    <tr v-for="employee in employees" :key="employee.id">
+      <td>{{employee.id}}</td>
+      <td>{{employee.name}}</td>
+      <td>{{employee.gender}}</td>
+      <td>{{employee.age}}</td>
+      <td>{{employee.email}}</td>
+    </tr>
+</table>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -13,20 +27,30 @@ export default {
   }
   },
   methods: {
-   getData(){
-     debugger
-     console.log("name");
-   },
-    getEmployeeData() {
-      debugger
-      fetch("db.json")
-        .then(response => response.json())
-        .then(data => (this.employees = data));
-  },
-  
-} ,
+  getEmployeeData() {
+      // debugger
+      // fetch("db.json")
+      //   .then(response => response.json())
+      //   .then(data => (this.employees = data.employees));
+     axios.get("db.json").then(response => (this.employees = response.data.employees));  
+  }
+},
 created() {
   this.getEmployeeData()
-  },
 }
+}
+
 </script>
+
+<style>
+table, td, th {  
+  border: 1px solid #333;
+  text-align: left;
+}
+table {
+  border-collapse: collapse;
+}
+tr, td {
+  padding: .5em;
+}
+</style>
